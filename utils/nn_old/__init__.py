18fe31ae8a97fbcb0626c9134b50f5fbef087640
Jirayu Kaewsing <strixz.self@gmail.com>
2025-09-24 02:08:19 +0700

    Remove old nn module

--- utils/nn_old/__init__.py
+++
@@ -1,7 +0,0 @@
-__all__ = ['layer', 'neuron', 'variable', 'sequential', 'initializer']
-
-from . import layer
-from . import neuron
-from . import variable
-from . import sequential
-from . import initializer
